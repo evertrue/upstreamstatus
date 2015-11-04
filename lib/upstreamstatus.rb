bb073cfab654f6f6ebe34544d1b9001752ed83d9
Eric Herot <eric.github@herot.com>
2015-11-04 15:02:49 -0500

    Add PagerDuty and Sentry notifications

--- lib/upstreamstatus.rb
+++ lib/upstreamstatus.rb
@@ -2,34 +2,50 @@ require 'upstreamstatus/version'
 require 'unirest'
 require 'yaml'
 require 'ostruct'
+require 'trollop'
 require 'forwardable'
+require 'json'
 
 class Upstreamstatus
   extend Forwardable
 
   def_delegators :@conf,
-                 :status_check_url
+                 :status_check_url,
+                 :sentry_dsn,
+                 :pagerduty_api_key
+
+  attr_reader :conf
 
   def initialize
     @conf = OpenStruct.new load_conf
+
+    return unless opts[:notify] && sentry_dsn
+    Raven.configure do |config|
+      config.dsn = sentry_dsn
+      config.logger = logger
+    end
   end
 
   def run
     down_hosts =
       current_status['servers']['server'].select { |s| s['status'] != 'up' }
 
-    exit 0 if down_hosts.empty?
-    print_hosts down_hosts
-    exit 1
-  end
-
-  private
-
-  def print_hosts(hosts)
-    hosts.each do |host|
-      host.each { |k, v| puts "#{k}: #{v}" }
-      puts
+    if down_hosts.empty?
+      clear_active_alerts!
+      exit 0
     end
+
+    print_hosts down_hosts
+    logger.notice "Detected down hosts: #{down_hosts.to_json}"
+
+    if opts[:notify]
+      puts 'Sending notifications'
+      notify(
+        'One or more API upstream hosts listed as down',
+        JSON.pretty_generate(down_hosts)
+      )
+    end
+    exit 1
   end
 
   def current_status
@@ -42,6 +58,68 @@ class Upstreamstatus
     end
 
     r.body
+  end
+
+  private
+
+  def clear_active_alerts!
+    return unless File.exist? '/var/run/active_upstream_alert'
+    File.delete('/var/run/active_upstream_alert')
+  end
+
+  def logger
+    @logger ||= Logger.new(conf['log']).tap { |l| l.progname = 'upstreamstatus' }
+  end
+
+  def pagerduty
+    @pagerduty ||= Pagerduty.new pagerduty_api_key
+  end
+
+  def notify(msg, details)
+    unless active_alert?(msg)
+      pd = pagerduty.trigger(
+        msg,
+        client: ENV['hostname'],
+        details: details
+      )
+
+      # You may have noticed that this will overwrite any previously active
+      # alerts, regardless of whether they are the "same" alert. This is a known
+      # limitation.
+
+      File.write(
+        '/var/run/active_upstream_alert',
+        {
+          'Incident Key' => pd.incident_key,
+          'Message' => msg,
+          'Details' => details
+        }.to_json
+      )
+    end
+
+    Raven.capture_exception(e) if sentry_dsn
+  end
+
+  def active_alert?(msg)
+    File.exist?('/var/run/active_upstream_alert') &&
+      (Time.now - File.ctime('/var/run/active_upstream_alert')) < 3600 &&
+      JSON.parse(File.read('/var/run/active_upstream_alert'))['msg'] == msg
+  end
+
+  def opts
+    @opts ||= Trollop.options do
+      opt :notify,
+          'Notify alert service on failure',
+          short: '-n',
+          default: false
+    end
+  end
+
+  def print_hosts(hosts)
+    hosts.each do |host|
+      host.each { |k, v| puts "#{k}: #{v}" }
+      puts
+    end
   end
 
   def load_conf
--- upstreamstatus.gemspec
+++ upstreamstatus.gemspec
@@ -27,7 +27,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'unirest'
+  spec.add_dependency 'unirest', '~> 1.1'
+  spec.add_dependency 'trollop', '~> 2.1'
 
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'rake', '~> 10.0'
